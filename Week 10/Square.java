393a3a49b296c03b9addb19aee82d0645334fd58
Wanna101 <78103741+Wanna101@users.noreply.github.com>
2022-11-02 23:21:06 -0600

    Add files via upload

---
+++ Week 10/Square.java
@@ -0,0 +1,22 @@
+public class Square {
+    private int rankIndex;
+    private int fileIndex;
+
+    public int getRankIndex() {
+        return rankIndex;
+    }
+
+    public void setRankIndex(int rankIndex) {
+        this.rankIndex = rankIndex;
+    }
+
+    public int getFileIndex() {
+        return fileIndex;
+    }
+
+    public void setFileIndex(int fileIndex) {
+        this.fileIndex = fileIndex;
+    }
+
+
+}
